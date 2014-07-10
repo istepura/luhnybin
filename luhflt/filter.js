ee006a6c054128caffaea9ccce09651b7910f043
Igor Stepura <igor.stepura@gmail.com>
2014-07-10 17:41:01 +0900

    Try to squeeze performance

--- luhflt/filter.js
+++ luhflt/filter.js
@@ -20,7 +20,7 @@ exports.LuhnFilter = LuhnFilter;
 
 var FilterState = function(){
 };
-FilterState.prototype.isnum =  function(char){
+var isnum =  function(char){
         return /^[0-9]$/i.test(char);
 };
 
@@ -36,7 +36,7 @@ FilterState.prototype.consumeChar = function(ctx, char){
 
 var FilterStateInit = function(){
     this.consumeChar =function(ctx, char){
-        if (this.isnum(char)) {
+        if (isnum(char)) {
             ctx.enterState('gather', char);
         }
         else {
@@ -56,46 +56,52 @@ var FilterStateGather = function(){
 };
 
 FilterStateGather.prototype = new FilterState();
-FilterStateGather.prototype.isgood = function(char){
-    return this.isnum(char) || (/^[\ -]/.test(char));
+
+var isgood = function(char){
+    return isnum(char) || (/^[\ -]/.test(char));
 };
 /* 
  * The idea is to store sequential digits in a buffer 
  * and keep track of ranges {l:r} which need to be 'X'-ed out.
  */
 FilterStateGather.prototype.consumeChar= function(ctx, char){
-    if (this.isgood(char)) {
+    var ranges = this.ranges;
+    if (isgood(char)) {
         this.buf += char;
 
         var doublesums = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
         var sumflag = this.sumflag;
-        if (this.isnum(char)){
+        var thisright = this.buf.length - 1;
+        var sumarray = this.sums[sumflag];
+        if (isnum(char)){
             var val = parseInt(char);
-            this.sums[sumflag].push(this.sums[sumflag][this.digitcnt] + val);
+
+            sumarray.push(sumarray[this.digitcnt] + val);
             this.sums[sumflag ^ 1].push(this.sums[sumflag ^ 1][this.digitcnt] + doublesums[val]);
+
             this.digitcnt += 1;
-            this.bufpos.push(this.buf.length- 1);
+            this.bufpos.push(thisright);
             this.sumflag ^= 1;
         }
 
         if (this.digitcnt >= 14){
 
             var digcount = this.digitcnt;
-            var thisright = this.buf.length - 1;
+
             [14, 15, 16].forEach(function(codelen){
                 if (digcount >= codelen){
-                    sum = this.sums[sumflag][digcount] - this.sums[sumflag][digcount - codelen];
+                    sum = sumarray[digcount] - sumarray[digcount - codelen];
                     //console.log(codelen, this.ranges, thisright, sum, sum % 10, this.sums, thisleft, this.bufpos);
                     if (sum % 10 == 0){
                         var thisleft = this.bufpos[digcount - codelen];
-                        var lastrange = this.ranges.length - 1;
+                        var lastrange = ranges.length - 1;
 
                         //Join the ranges if overlap
-                        if ( (lastrange >= 0) && (this.ranges[lastrange].r >= thisleft)){
-                            this.ranges[lastrange].r = thisright;
-                            this.ranges[lastrange].l = Math.min(this.ranges[lastrange].l, thisleft);
+                        if ( (lastrange >= 0) && (ranges[lastrange].r >= thisleft)){
+                            ranges[lastrange].r = thisright;
+                            ranges[lastrange].l = Math.min(ranges[lastrange].l, thisleft);
                         }else{
-                            this.ranges.push({l: thisleft, r : thisright});
+                            ranges.push({l: thisleft, r : thisright});
                         }
                     }
                 }
@@ -106,7 +112,7 @@ FilterStateGather.prototype.consumeChar= function(ctx, char){
         /*
          * Dump everything we had in buffer, masking ranges
          */
-       this.ranges.forEach(function(val){
+       ranges.forEach(function(val){
             this.buf = this.buf.replace(/[0-9]/g, function(match, offset, string){
                 if ((val.l <= offset) && (val.r >= offset)){
                     return 'X';
